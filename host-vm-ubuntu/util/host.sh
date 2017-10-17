390c2465a9ee81a0101db45f8bb965186a9fb230
Franklin Pazmino <30947643+jmpaz@users.noreply.github.com>
2017-10-17 15:39:59 -0400

    Move scripts to Dropbox

--- host-vm-ubuntu/util/host.sh
+++ host-vm-ubuntu/util/host.sh
@@ -4,5 +4,7 @@ export DWALLS="$HOME/Shared/Dropbox/Wallpapers"
 
 alias logout="gnome-session-quit"
 
+export PATH=$PATH:~/Shared/Dropbox/bin
 export PATH=$PATH:~/.npm-global/bin
+
 source $HOME/.rvm/scripts/rvm
