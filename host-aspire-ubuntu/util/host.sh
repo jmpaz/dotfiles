ea592b401900ff69f093a586fde2e48fe31766b2
Franklin Pazmino <0jfpz0@gmail.com>
2018-07-23 15:08:10 -0400

    Switch to dropbox from rclone

--- host-aspire-ubuntu/util/host.sh
+++ host-aspire-ubuntu/util/host.sh
@@ -1,6 +1,6 @@
 alias logout="gnome-session-quit"
 export PATH=$PATH:~/.npm-global/bin
 export PATH=$PATH:~/Dropbox/bin
-export DBIN="$HOME/.bin"
-export DWALLS="$HOME/Pictures/Wallpapers"
+export DBIN="$HOME/Dropbox/bin"
+export DWALLS="$HOME/Dropbox/Wallpapers"
 source $HOME/.rvm/scripts/rvm
