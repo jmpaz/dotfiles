c5fcfaf3041940ea87b3b4814d341d84a7e2c6d4
Franklin Pazmino <30947643+jmpaz@users.noreply.github.com>
2018-07-23 20:11:26 -0400

    Add HDMI check

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+
+# check if hdmi is connected
+if (xrandr | grep "HDMI-1 connected" > /dev/null)
+then
+    sleep 3
+fi
+
 killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
