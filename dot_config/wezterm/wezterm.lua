37e7683ed85a2b6ce43e1b69f2ceab9a26c74d29
jmpaz <30947643+jmpaz@users.noreply.github.com>
2023-11-13 21:53:42 -0500

    Update wezterm config https://github.com/theopn/dotfiles/blob/25b85936ef3e7195a0f029525f854fdb915b9f90/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,14 +1,26 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 return {
 	color_scheme = "Catppuccin Mocha",
 
 	default_cursor_style = "BlinkingBar",
 	animation_fps = 60,
-	font = wezterm.font("Geist Mono"),
+	font = wezterm.font_with_fallback({
+		{ family = "Geist Mono" },
+		{ family = "Monaspace Argon" },
+	}),
 	font_size = 15.2,
 
-	enable_tab_bar = false,
+	scrollback_lines = 10000,
+
+	inactive_pane_hsb = {
+		saturation = 0.55,
+		brightness = 0.45,
+	},
+
+	use_fancy_tab_bar = false,
+	window_decorations = "RESIZE",
 	window_background_opacity = 0.9,
 
 	window_padding = {
@@ -17,4 +29,101 @@ return {
 		top = 10,
 		bottom = 0,
 	},
+
+	leader = { key = "a", mods = "ALT", timeout_milliseconds = 1000 },
+	keys = {
+		{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+
+		{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+		{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+		{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+		{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+		{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+
+		{ key = "s", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+		{ key = "s", mods = "LEADER|ALT", action = act.RotatePanes("CounterClockwise") },
+
+		{ key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+		{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+		{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+		{ key = "t", mods = "LEADER", action = act.ShowTabNavigator },
+		{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+
+		{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+
+		-- Key tables
+		{
+			key = "r",
+			mods = "LEADER",
+			action = act.ActivateKeyTable({ name = "resize", one_shot = false, timeout_milliseconds = 2000 }),
+		},
+		{
+			key = "m",
+			mods = "LEADER",
+			action = act.ActivateKeyTable({ name = "shift", one_shot = false, timeout_milliseconds = 2000 }),
+		},
+	},
+
+	key_tables = {
+		resize = {
+			{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+			{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+			{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+			{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+			{ key = "Escape", action = "PopKeyTable" },
+			{ key = "Enter", action = "PopKeyTable" },
+		},
+		shift = {
+			{ key = "h", action = act.MoveTabRelative(-1) },
+			{ key = "j", action = act.MoveTabRelative(-1) },
+			{ key = "k", action = act.MoveTabRelative(1) },
+			{ key = "l", action = act.MoveTabRelative(1) },
+			{ key = "Escape", action = "PopKeyTable" },
+			{ key = "Enter", action = "PopKeyTable" },
+		},
+	},
+
+	-- Status bar
+	wezterm.on("update-right-status", function(window, pane)
+		-- Workspace name
+		local indicator = window:active_workspace()
+
+		-- Leader/key table indicator
+		if window:active_key_table() then
+			indicator = window:active_key_table()
+		end
+
+		if window:leader_is_active() then
+			indicator = wezterm.nerdfonts.md_apple_keyboard_command .. " "
+		end
+
+		-- Current directory
+		local basename = function(s)
+			-- Get the last segment of the path with regex
+			return string.gsub(s, "(.*[/\\])(.*)", "%2")
+		end
+
+		local cwd = basename(pane:get_current_working_dir())
+
+		-- Current process, timestamp
+		local cmd = basename(pane:get_foreground_process_name())
+		local time = wezterm.strftime("%H:%M")
+
+		-- Set the status
+		window:set_right_status(wezterm.format({
+			{ Text = indicator },
+			{ Text = " | " },
+			{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+			{ Text = " | " },
+			{ Foreground = { Color = "FFB86C" } },
+			{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+			"ResetAttributes",
+			{ Text = " | " },
+			{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+			{ Text = " |" },
+		}))
+	end),
 }
