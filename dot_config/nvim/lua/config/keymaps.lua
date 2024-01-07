22d787a2edf3371d8f22da07022ddeff70a7cc49
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-01-06 21:18:10 -0500

    Add `nvim-osc52`

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-CR>", "za", { desc = "Toggle fold" })
+
+-- OSC52
+vim.keymap.set("n", "<leader>+", require("osc52").copy_operator, { expr = true })
+vim.keymap.set("n", "<leader>++", "<leader>c_", { remap = true })
+vim.keymap.set("v", "<leader>+", require("osc52").copy_visual)
--- dot_config/nvim/lua/plugins/enabled.lua
+++ dot_config/nvim/lua/plugins/enabled.lua
@@ -1,39 +1,45 @@
 return {
-  {
-    "gen740/SmoothCursor.nvim",
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require("smoothcursor").setup({
-        speed = 20,
-        intervals = 20,
-      })
-    end,
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup()
-    end,
-  },
-  {
-    "David-Kunz/gen.nvim",
-    opts = {
-      model = "mistral", -- The default model to use.
-      display_mode = "float", -- The display mode. Can be "float" or "split".
-      show_prompt = false, -- Shows the Prompt submitted to Ollama.
-      show_model = false, -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false, -- Never closes the window automatically.
-      init = function(options)
-        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
-      end,
-      -- Function to initialize Ollama
-      command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
-      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-      -- This can also be a lua function returning a command string, with options as the input parameter.
-      -- The executed command must return a JSON object with { response, context }
-      -- (context property is optional).
-      list_models = "<omitted lua function>", -- Retrieves a list of model names
-      debug = false, -- Prints errors and the command which is run.
-    },
-  },
+	{
+		"gen740/SmoothCursor.nvim",
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("smoothcursor").setup({
+				speed = 20,
+				intervals = 20,
+			})
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+	{
+		"ojroques/nvim-osc52",
+		config = function()
+			require("osc52").setup()
+		end,
+	},
+	{
+		"David-Kunz/gen.nvim",
+		opts = {
+			model = "mistral", -- The default model to use.
+			display_mode = "float", -- The display mode. Can be "float" or "split".
+			show_prompt = false, -- Shows the Prompt submitted to Ollama.
+			show_model = false, -- Displays which model you are using at the beginning of your chat session.
+			no_auto_close = false, -- Never closes the window automatically.
+			init = function(options)
+				pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+			end,
+			-- Function to initialize Ollama
+			command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
+			-- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+			-- This can also be a lua function returning a command string, with options as the input parameter.
+			-- The executed command must return a JSON object with { response, context }
+			-- (context property is optional).
+			list_models = "<omitted lua function>", -- Retrieves a list of model names
+			debug = false, -- Prints errors and the command which is run.
+		},
+	},
 }
