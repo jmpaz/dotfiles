4e4875a2f3afca18398f714c55967ee46faf243d
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-01-09 13:11:00 -0500

    Add .envrc creation to `venv`

--- dot_config/private_fish/functions/venv.fish
+++ dot_config/private_fish/functions/venv.fish
@@ -1,10 +1,19 @@
+# Creates or activates a Python virtual environment in the current directory
+
 function venv
+    # Check for the --no_direnv argument to determine if .envrc should be created
+    set no_direnv_flag 0
+    for arg in $argv
+        if test "$arg" = --no_direnv
+            set no_direnv_flag 1
+            break
+        end
+    end
+
     if test -d ".venv"
-        # echo ".venv exists"
         source .venv/bin/activate.fish
         echo "Virtual environment activated."
     else if test -d venv
-        echo "venv exists"
         source venv/bin/activate.fish
         echo "Virtual environment activated."
     else
@@ -15,8 +24,18 @@ function venv
                 echo "Creating a new virtual environment..."
                 python -m venv .venv --system-site-packages
                 echo "Virtual environment created."
-                source .venv/bin/activate.fish
-                echo "Virtual environment activated."
+
+                if test $no_direnv_flag -eq 0
+                    # If --no_direnv was not passed, create and allow .envrc file
+                    echo 'export VIRTUAL_ENV=./.venv' >.envrc
+                    echo 'export PATH=./.venv/bin:$PATH' >>.envrc
+                    echo ".envrc file created."
+                    direnv allow
+                else
+                    # Activate the environment immediately if --no_direnv was passed
+                    source .venv/bin/activate.fish
+                    echo "Virtual environment activated."
+                end
             case '*'
                 echo "Virtual environment creation cancelled."
         end
