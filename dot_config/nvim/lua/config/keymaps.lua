dd545ba95a283811b829c060ad44daebe3f3fd55
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-02-15 13:16:57 -0500

    Change localleader to `,`

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "<C-CR>", "za", { desc = "Toggle fold" })
 
 -- OSC52
 vim.keymap.set("n", "<leader>+", require("osc52").copy_operator, { expr = true })
