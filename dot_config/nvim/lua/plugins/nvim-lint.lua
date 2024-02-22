0222bd2dab5af71b77883bfecc7b97bdceb1dd94
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-02-22 17:14:41 -0500

    Disable markdown linting entirely

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -1,34 +1,12 @@
 return {
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters = {
-        markdownlint = {
-          args = {
-            "--disable",
-            "MD003",
-            "--disable",
-            "MD010",
-            "--disable",
-            "MD012",
-            "--disable",
-            "MD013",
-            "--disable",
-            "MD022",
-            "--disable",
-            "MD031",
-            "--disable",
-            "MD032",
-            "--disable",
-            "MD034",
-            "--disable",
-            "MD041",
-            "--disable",
-            "MD047",
-            "--",
-          },
-        },
-      },
-    },
-  },
+	{
+		"mfussenegger/nvim-lint",
+		opts = {
+			linters = {
+				markdownlint = {
+					args = { "--disable", "all" },
+				},
+			},
+		},
+	},
 }
