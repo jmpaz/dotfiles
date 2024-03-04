ee1a5bd1a7bc02280d4e49942ce47306367139f0
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-03-04 15:31:59 -0500

    Switch from LazyVim to kickstart-modular.nvim
    https://github.com/jmpaz/nvim
    https://github.com/dam9000/kickstart-modular.nvim

--- dot_config/nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-
--- OSC52
-vim.keymap.set("n", "<leader>+", require("osc52").copy_operator, { expr = true })
-vim.keymap.set("n", "<leader>++", "<leader>c_", { remap = true })
-vim.keymap.set("v", "<leader>+", require("osc52").copy_visual)
