e4cfce4d907934044685efa9666c3d4b6d0cdf7f
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-09-10 15:16:58 -0400

    Update venv.fish

--- dot_config/private_fish/functions/venv.fish
+++ dot_config/private_fish/functions/venv.fish
@@ -1,10 +1,13 @@
-function venv --description "create or activate a Python virtual environment in the current directory"
-    # Check for the --no_direnv argument to determine if .envrc should be created
+function venv --description "create, activate, or reinitialize a Python virtual environment in the current directory"
     set no_direnv_flag 0
+    set reinit_flag 0
+
     for arg in $argv
-        if test "$arg" = --no_direnv
-            set no_direnv_flag 1
-            break
+        switch $arg
+            case --no_direnv
+                set no_direnv_flag 1
+            case --reinit
+                set reinit_flag 1
         end
     end
 
@@ -14,18 +17,95 @@ function venv --description "create or activate a Python virtual environment in 
         echo "Virtual environment activated."
     end
 
-    function create_pyproject_toml
-        echo "[tool.pyright]" > pyproject.toml
-        echo 'venvPath = "."' >> pyproject.toml
-        echo 'venv = ".venv"' >> pyproject.toml
-        echo "pyproject.toml file created."
+    function set_pyproject_toml
+        if not test -f pyproject.toml
+            echo "[tool.pyright]" > pyproject.toml
+            echo 'venvPath = "."' >> pyproject.toml
+            echo 'venv = ".venv"' >> pyproject.toml
+            echo "pyproject.toml file created."
+        else
+            set pyright_section 0
+            set venv_path_set 0
+            set venv_set 0
+            while read -l line
+                if string match -q "[tool.pyright]" $line
+                    set pyright_section 1
+                else if test $pyright_section -eq 1
+                    if string match -q 'venvPath = "."' $line
+                        set venv_path_set 1
+                    else if string match -q 'venv = ".venv"' $line
+                        set venv_set 1
+                    end
+                end
+            end < pyproject.toml
+
+            if test $pyright_section -eq 0
+                echo "" >> pyproject.toml
+                echo "[tool.pyright]" >> pyproject.toml
+            end
+            if test $venv_path_set -eq 0
+                echo 'venvPath = "."' >> pyproject.toml
+            end
+            if test $venv_set -eq 0
+                echo 'venv = ".venv"' >> pyproject.toml
+            end
+            echo "pyproject.toml file updated."
+        end
     end
 
-    function create_envrc
-        echo 'export VIRTUAL_ENV=.venv' > .envrc
-        echo 'layout python' >> .envrc
-        echo ".envrc file created."
-        direnv allow
+    function set_envrc
+        if not test -f .envrc
+            echo 'export VIRTUAL_ENV=.venv' > .envrc
+            echo 'layout python' >> .envrc
+            echo ".envrc file created."
+            direnv allow
+        else
+            set virtual_env_set 0
+            set layout_python_set 0
+            while read -l line
+                if string match -q 'export VIRTUAL_ENV=.venv' $line
+                    set virtual_env_set 1
+                else if string match -q 'layout python' $line
+                    set layout_python_set 1
+                end
+            end < .envrc
+
+            if test $virtual_env_set -eq 0
+                echo 'export VIRTUAL_ENV=.venv' >> .envrc
+            end
+            if test $layout_python_set -eq 0
+                echo 'layout python' >> .envrc
+            end
+            echo ".envrc file updated."
+            direnv allow
+        end
+    end
+
+    if test $reinit_flag -eq 1
+        if set -q VIRTUAL_ENV
+            set -e VIRTUAL_ENV
+            set -e PATH
+            set -x PATH $PATH
+        end
+        if test -d ".venv"
+            rm -rf .venv
+            echo "Existing .venv directory removed."
+        end
+        echo "Reinitializing virtual environment..."
+        uv venv
+        echo "Virtual environment reinitialized."
+
+        if test $no_direnv_flag -eq 0
+            set_envrc
+        else
+            activate_venv .venv
+        end
+
+        set_pyproject_toml
+
+        # install packages
+        uv pip install jupyter_client ipykernel pynvim
+        return
     end
 
     if test -d ".venv"
@@ -42,12 +122,12 @@ function venv --description "create or activate a Python virtual environment in 
                 echo "Virtual environment created."
 
                 if test $no_direnv_flag -eq 0
-                    create_envrc
+                    set_envrc
                 else
                     activate_venv .venv
                 end
 
-                create_pyproject_toml
+                set_pyproject_toml
 
                 # install packages
                 uv pip install jupyter_client ipykernel pynvim
