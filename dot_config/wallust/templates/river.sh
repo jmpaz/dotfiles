cd4382f7e5747403879a0f8e55411098fbee79ca
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-11-25 23:48:28 -0500

    Switch layout generator to wideriver from rivercarro

--- dot_config/wallust/templates/river.sh
+++ dot_config/wallust/templates/river.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-riverctl background-color 0x{{color2 | blend(color0) | blend(color0) | strip}}
-riverctl border-color-focused 0x{{color4 | saturate(-0.2) | strip}}
-riverctl border-color-unfocused 0x{{color8 | strip}}
+BACKGROUND=0x{{color2 | blend(color0) | blend(color0) | strip}}
+FOCUSED=0x{{color4 | saturate(-0.2) | strip}}
+UNFOCUSED=0x{{color8 | strip}}
+
+riverctl background-color $BACKGROUND
+riverctl border-color-focused $FOCUSED
+riverctl border-color-unfocused $UNFOCUSED
+
+export RIVER_BACKGROUND=$BACKGROUND
+export RIVER_FOCUSED=$FOCUSED
+export RIVER_UNFOCUSED=$UNFOCUSED
