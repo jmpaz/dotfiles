d48a2718550ced7e74cb88f03e71a689c042c439
jmpaz <30947643+jmpaz@users.noreply.github.com>
2025-10-23 22:10:02 -0400

    tmux: Sanitize popup session name

--- dot_config/tmux/scripts/executable_popup.sh
+++ dot_config/tmux/scripts/executable_popup.sh
@@ -2,16 +2,17 @@
 
 if [ -n "$TMUX" ]; then
     current_window=$(tmux display-message -p '#{window_id}')
-    popup_session="popup_${current_window}"
+    popup_session="popup_${current_window#@}"
     current_session=$(tmux display-message -p '#{session_name}')
+    popup_path=$(tmux display-message -p -F '#{pane_current_path}')
 
     if [[ "$current_session" =~ ^popup_ ]]; then
         tmux detach-client || exit 1
     else
         if ! tmux has-session -t "$popup_session" 2>/dev/null; then
-            tmux new-session -d -s "$popup_session" -c '#{pane_current_path}' || exit 1
+            tmux new-session -ds "$popup_session" -c "$popup_path" || exit 1
         fi
-        tmux popup -d '#{pane_current_path}' -xC -yC -w80% -h80% -E "tmux attach-session -t $popup_session" || exit 1
+        tmux popup -d "$popup_path" -xC -yC -w80% -h80% -E "tmux attach-session -t \"$popup_session\"" || exit 1
     fi
 else
     echo "Not in a tmux session" >&2
